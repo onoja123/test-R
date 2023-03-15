a80e5cb7b48dd90a9e16e339879b146b0a100098
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2023-03-16 00:39:46 +0100

    little changes

--- index.r
+++ index.r
@@ -1,1 +1,35 @@
+#print hello world
+
 "Hello world"
+
+#print hello world using print
+print("hello world")
+
+#naming vars
+name <- "Godwin"
+name
+
+#or using print
+print(name)
+
+age <- 40
+age
+
+#or using print
+print(age)
+
+#concatenating variables
+class <- 3
+print("he is in jss", class)
+
+first <- "R is"
+second <- "awesome"
+
+paste(first, second)
+
+#multiple variable
+
+var1 <- var2 <- var3 <- "welcome"
+var1
+var2
+var3
