a6f33aebe96ee6d5f06de630e985e07e45aa9b0e
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2023-03-16 00:41:28 +0100

    Create

---
+++ data.r
@@ -0,0 +1,19 @@
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 300K
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "He is exciting"
+class(x)
+
+# logical
+x <- TRUE
+class(x)
