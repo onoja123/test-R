fbbd09a197886f7db963b7e0655605001ccce8df
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2023-03-31 21:00:32 +0100

    Update conditional.r

--- conditional.r
+++ conditional.r
@@ -1,1 +1,23 @@
+a <- 33
+b <- 200
 
+# Using if only
+if (b > a) {
+  print("b is greater than a")
+}
+
+# Using if and else if
+if (b > a) {
+  print("b is greater than a")
+} else if (a == b) {
+  print ("a and b are equal")
+}
+
+# Using if, else if and else
+if (b > a) {
+  print("b is greater than a")
+} else if (a == b) {
+  print("a and b are equal")
+} else {
+  print("a is greater than b")
+}
